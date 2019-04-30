d921f8a2c247ae3a63bf04d55891a89bc7bfde8a
Anthony Rurup <tcrurup@gmail.com>
2019-04-30 18:59:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,11 +5,16 @@ class Dog
   def initialize(name)
     
     @name = name
-    
+    @all = 
   end
   
   def self.clear_all
     @@all.clear
   end
   
+  
+  def self.all
+    
+    @all.each do |STDIN
+  end
 end
